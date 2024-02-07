680ab5082d4bddf96dc1f2febe2495dddc38cbe1
gaurav <gmethwani365@gmail.com>
2024-02-08 03:35:08 +0530

    commit all

--- server.js
+++ server.js
@@ -6,20 +6,7 @@ const hostname = "127.0.0.1";
 const port = 3000;
 // Create the server
 http.createServer((req, res) => {
-    const URL= url.parse(req.url);
-    console.log(URL.pathname);
-    if (URL.pathname == "/home") {
-      res.write("home page");
-      res.end();
-    } else if (URL.pathname == "/about") {
-      res.write("About page");
-      res.end();
-    } else if (URL.pathname == "/contact-us") {
-      res.write("Contact page");
-      res.end();
-    } else {
-      res.write("404 Not Found");
-      res.end();
-    }
+    res.write("Home-Page");
+    res.end();
   })
   .listen(port);
